9540952443e6a3ed94fbe3fba15d6ecebdf64b06
asakofujiki <a.fujiki1554@gmail.com>
2020-05-04 16:59:23 +0900

    応用課題３終了

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'home#top'
   devise_for :users
-  resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+  resources :users, only: [:show,:index,:edit,:update]
+  resources :books do
+  	resources :book_comments, only: [:create, :destroy]
+  	resource :favorites, only: [:create, :destroy]
+  end
   get 'home/about' => 'home#about'
 end
